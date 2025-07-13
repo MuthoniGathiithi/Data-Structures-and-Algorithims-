dd64743a7e1afec62ea22c60bb8637e84fc8f048
Joyce <gathiithijoyce74@gmail.com>
2025-07-14 00:16:36 +0300

    created a file in array data structures and algorithims print numbers with loops

---
+++ print_all_names_with_loops.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main () {
+    string animals [] = {"Goat", "Pig","Cow"};
+
+    for (int i =0; i < 3;i++){
+    cout << animals [i] << endl;
+    }
+    
+    return 0;
+}
